138c7aa2880d1c6679b39863e10f3a12c6d3cb29
Ivan <shuvalol.vana@gmail.com>
2023-01-26 21:31:08 +0200

    task27 done

---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int summa_cifr(int a)
+{
+    int ost;
+    int result=0;
+    for (int i =0; a>0;i++)
+    {
+        ost=a%10;
+        a=a/10;
+        result=result+ost;
+    }
+    return result;
+}
+Console.WriteLine("Введите число:");
+int chislo = int.Parse(Console.ReadLine()!);
+int sum = summa_cifr(chislo);
+Console.WriteLine($"{chislo} -> {sum}");
